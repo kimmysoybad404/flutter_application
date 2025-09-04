8e6a7a0e1ad7b8d29864371599708b48075734ab
Kimmyoybad <6631501008@lamduan.mfu.ac.th>
2025-09-04 14:53:44 +0700

    add basic app code

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application/week01/basic.dart';
+
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text('My first app')),
-        body: Text('Hello world'),
-        floatingActionButton: FloatingActionButton
-        (onPressed: null, child: Text('Click'),),
-      ),
+  runApp(MaterialApp(home: 
+      Basic()
     ),
   );
 }
