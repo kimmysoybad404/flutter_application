40b0af8e4b830b3d3357a6426a3bae2ee1cab158
Kimmysoybad <6631501008@lamduan.mfu.ac.th>
2025-09-04 16:01:57 +0700

    Create a counter

--- lib/week01/counter.dart
+++ lib/week01/counter.dart
@@ -8,20 +8,34 @@ class Counter extends StatefulWidget {
 }
 
 class _CounterState extends State<Counter> {
-  String message = 'Hello';
+  int count = 0;
+
+  void update() {
+    // debugPrint(message);
+    setState(() {
+      count++;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Counter')),
-      body: Text(message),
+      appBar: AppBar(
+        title: Text('Counter', style: TextStyle(color: Colors.white)),
+        backgroundColor: Colors.blue,
+      ),
+      body: Center(
+        child: Text(
+          'Count = $count',
+          style: TextStyle(
+            fontSize: 26,
+            color: Color.fromARGB(255, 0, 30, 129),
+          ),
+        ),
+      ),
+
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // debugPrint(message);
-          setState(() {
-            message == 'Hello' ? message = 'Hi' : message = 'Hello';
-          
-          });
-        },
+        onPressed: update,
         child: Text('Click'),
       ),
     );
