ef3046f222b0ddf68dc5292ccb658c2937270719
kimmysoybad404 <6631501008@lamduan.mfu.ac.th>
2025-09-11 16:15:09 +0700

    design2 complete

--- lib/week05/design2_demo.dart
+++ lib/week05/design2_demo.dart
@@ -5,6 +5,51 @@ class Design2Demo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Profile', style: TextStyle(color: Colors.white)),
+        backgroundColor: Colors.black,
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.grey[800],
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // https://www.iconarchive.com/download/i106639/diversity-avatars/avatars/batman.128.png
+            
+            Center(
+              child: CircleAvatar(
+                radius: 40,
+                backgroundColor: Colors.blue,
+                backgroundImage: NetworkImage(
+                  "https://www.iconarchive.com/download/i106639/diversity-avatars/avatars/batman.128.png",
+                ),
+              ),
+            ),
+                      Divider(color: Colors.grey, height: 60,),
+            Text('Name', style: TextStyle(color: Colors.grey[400])),
+            Text(
+              'John Doe',
+              style: TextStyle(color: Colors.yellow, fontSize: 22),
+            ),
+            SizedBox(width: 16,),
+            Text('Age', style: TextStyle(color: Colors.grey[400])),
+            Text('22', style: TextStyle(color: Colors.yellow, fontSize: 22)),
+            SizedBox(width: 16,),
+            Row(
+              children: [
+                Icon(Icons.email, color: Colors.grey),
+                Text(
+                  'john_doe@test.com',
+                  style: TextStyle(color: Colors.grey[400]),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
