3a13f4d0f3403cf81e566d0f77dca429a64e1fd6
kimmysoybad404 <6631501008@lamduan.mfu.ac.th>
2025-09-11 15:34:52 +0700

    Expanded

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:flutter_application/week05/multi_child_demo.dart';
 void main() {
   runApp(MaterialApp(home: 
       MultiChildDemo(), debugShowCheckedModeBanner: false,
+
     ),
   );
 }
--- lib/week05/multi_child_demo.dart
+++ lib/week05/multi_child_demo.dart
@@ -8,10 +8,19 @@ class MultiChildDemo extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
         child: Row(
+          // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Container(color: Colors.green, width: 100, height: 100),
+            Expanded(
+              flex: 2,
+              child:  Container(color: Colors.green, width: 100, height: 100),
+            ),
             Container(color: Colors.yellow, width: 100, height: 150),
-            Container(color: Colors.red, width: 100, height: 100),
+            Expanded(
+              flex: 2,
+              child:  Container(color: Colors.red, width: 100, height: 100),
+            ),
+            
             
           ],
         ),
