d2937799eebffa86feb409024d4913ab89b7b670
kimmysoybad404 <6631501008@lamduan.mfu.ac.th>
2025-09-11 15:09:31 +0700

    change main

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application/week05/week5.dart';
+import 'package:flutter_application/week05/multi_child_demo.dart';
 
 
 void main() {
   runApp(MaterialApp(home: 
-      Week5(), debugShowCheckedModeBanner: false,
+      MultiChildDemo(), debugShowCheckedModeBanner: false,
     ),
   );
 }
