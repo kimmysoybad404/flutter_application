27b5e3991350fa45390df49bf905d2891fab253c
kimmysoybad404 <6631501008@lamduan.mfu.ac.th>
2025-09-11 14:45:03 +0700

    Add containner

--- lib/week05/week5.dart
+++ lib/week05/week5.dart
@@ -5,6 +5,10 @@ class Week5 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(
+        child: Text('Singlechild'),
+      ),
+    );
   }
 }
