fb2e81c3d3dab3638b77d7600d7dae5bf71f4800
kimmysoybad404 <6631501008@lamduan.mfu.ac.th>
2025-09-11 14:40:43 +0700

    change: stateful to stateless

--- lib/week05/week5.dart
+++ lib/week05/week5.dart
@@ -1,13 +1,8 @@
 import 'package:flutter/material.dart';
 
-class Week5 extends StatefulWidget {
+class Week5 extends StatelessWidget {
   const Week5({super.key});
 
-  @override
-  State<Week5> createState() => _Week5State();
-}
-
-class _Week5State extends State<Week5> {
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
