60eece23e31b51d1b73690bcc7d60f1490e8f9db
kimmysoybad404 <6631501008@lamduan.mfu.ac.th>
2025-09-11 15:51:22 +0700

    design2demo

---
+++ lib/week05/design2_demo.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Design2Demo extends StatelessWidget {
+  const Design2Demo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
