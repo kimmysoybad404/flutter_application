76d8817bb11bf35a551dcd9df98c343a18dd6771
kimmysoybad404 <6631501008@lamduan.mfu.ac.th>
2025-09-11 15:44:13 +0700

    design1demo

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application/week05/multi_child_demo.dart';
+import 'package:flutter_application/week05/design1_demo.dart';
 
 
 void main() {
   runApp(MaterialApp(home: 
-      MultiChildDemo(), debugShowCheckedModeBanner: false,
+      Design1Demo(), debugShowCheckedModeBanner: false,
 
     ),
   );
