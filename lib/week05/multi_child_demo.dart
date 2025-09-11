fda602dac181375c90230928fc97f1efa1fe7521
Torkitti <6631501011@lamduan.mfu.ac.th>
2025-09-11 15:40:48 +0700

    lala

--- lib/week05/multi_child_demo.dart
+++ lib/week05/multi_child_demo.dart
@@ -8,19 +8,12 @@ class MultiChildDemo extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
         child: Row(
-          // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.center,
-          //children: [
-           // Container(color: Colors.green, width: 100, height: 100),
-           // Spacer(flex: 1,),
-           // Container(color: Colors.white, width: 100, height: 100),
-          //  Spacer(flex: 2,),
-          //  Container(color: Colors.red, width: 100, height: 100),
           children: [
-            Expanded(child: Container(color: Colors.green, width: 100, height: 100)),
-            Expanded(child: Container(color: Colors.white, width: 100, height: 100)),
-            Expanded(child: Container(color: Colors.red, width: 100, height: 100)),
-            
+            Container(color: Colors.blue, width: 100, height: 100),
+            Container(color: Colors.white, width: 100, height: 100),
+            Container(color: Colors.red, width: 100, height: 100),
           ],
         ),
       ),
