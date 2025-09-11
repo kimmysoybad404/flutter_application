f18f6103a5d2660be79269bb136d359477020bad
kimmysoybad404 <6631501008@lamduan.mfu.ac.th>
2025-09-11 15:39:37 +0700

    Column

--- lib/week05/multi_child_demo.dart
+++ lib/week05/multi_child_demo.dart
@@ -7,20 +7,13 @@ class MultiChildDemo extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Row(
-          // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Expanded(
-              flex: 2,
-              child:  Container(color: Colors.green, width: 100, height: 100),
-            ),
-            Container(color: Colors.yellow, width: 100, height: 150),
-            Expanded(
-              flex: 2,
-              child:  Container(color: Colors.red, width: 100, height: 100),
-            ),
-            
+            Container(color: Colors.green, width: 100, height: 100,),
+            Container(color: Colors.yellow, width: 150, height: 100,),
+            Container(color: Colors.red, width: 100, height: 100,),
             
           ],
         ),
