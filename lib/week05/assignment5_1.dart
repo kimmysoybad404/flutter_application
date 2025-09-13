fcca4c1156f42f91d7318d336df2b8094f4ade29
Kimmysoybad <6631501008@lamduan.mfu.ac.th>
2025-09-13 16:03:35 +0700

    finish assignment 5.1

--- lib/week05/assignment5_1.dart
+++ lib/week05/assignment5_1.dart
@@ -11,8 +11,48 @@ class _Assignment51State extends State<Assignment51> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.pink
-      
+      backgroundColor: Colors.pink[200],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Spacer(),
+            Text(
+              'Wedding Organiser',
+              style: TextStyle(
+                color: Colors.white,
+                fontFamily: 'Sevillana',
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              'Pre-wedding, Photo, Party',
+              style: TextStyle(
+                color: Colors.white,
+                fontFamily: 'Sevillana',
+                fontSize: 30,
+              ),
+            ),
+            SizedBox(height: 25),
+            FilledButton(
+              onPressed: () {},
+              style: ButtonStyle(
+                backgroundColor: WidgetStatePropertyAll(Colors.red),
+              ),
+              child: Text('Our services'),
+            ),
+            Spacer(),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Text(
+                '345 Moo 1 Tasud Chiang Rai, Thailand',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
