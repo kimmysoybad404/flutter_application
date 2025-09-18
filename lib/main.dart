9ef6f24664798c8d4c830201d00313f7e133b13a
Kittinan <6631501009@lamduan.mfu.ac.th>
2025-09-18 13:10:59 +0700

    ass

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application/week05/Assignment2.dart';
 
-import 'package:flutter_application/week05/design2_demo.dart';
 
 
 
 
 void main() {
   runApp(MaterialApp(home: 
-      Design2Demo(), debugShowCheckedModeBanner: false,
+      Calculate(), debugShowCheckedModeBanner: false,
     ),
   );
 }
--- lib/week05/design2_demo.dart
+++ lib/week05/design2_demo.dart
@@ -6,17 +6,50 @@ class Design2Demo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[800],
       appBar: AppBar(
-        
-
-       title: const Text('My Profile', style: TextStyle(color: Colors.white),),
-       centerTitle: true,
-       backgroundColor: Colors.black,
-       centerTitle: true,
+        title: Text('My Profile', style: TextStyle(color: Colors.white)),
+        backgroundColor: Colors.black,
+        centerTitle: true,
       ),
-      
-
+      backgroundColor: Colors.grey[800],
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // https://www.iconarchive.com/download/i106639/diversity-avatars/avatars/batman.128.png
+            
+            Center(
+              child: CircleAvatar(
+                radius: 40,
+                backgroundColor: Colors.blue,
+                backgroundImage: NetworkImage(
+                  "https://www.iconarchive.com/download/i106639/diversity-avatars/avatars/batman.128.png",
+                ),
+              ),
+            ),
+                      Divider(color: Colors.grey, height: 60,),
+            Text('Name', style: TextStyle(color: Colors.grey[400])),
+            Text(
+              'John Doe',
+              style: TextStyle(color: Colors.yellow, fontSize: 22),
+            ),
+            SizedBox(height: 16,),
+            Text('Age', style: TextStyle(color: Colors.grey[400])),
+            Text('22', style: TextStyle(color: Colors.yellow, fontSize: 22)),
+            SizedBox(height: 16,),
+            Row(
+              children: [
+                Icon(Icons.email, color: Colors.grey),
+                Text(
+                  'john_doe@test.com',
+                  style: TextStyle(color: Colors.grey[400]),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
