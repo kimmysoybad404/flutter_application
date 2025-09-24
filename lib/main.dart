12cbf6f7604a9ee744caad42243c4a902a871370
Dinozexe <Kittamat34@gmail.com>
2025-09-25 00:21:12 +0700

    FixSomeThing

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application/W7/Page1.dart';
 
 void main() {
-  runApp(MaterialApp(home: Scaffold(), debugShowCheckedModeBanner: false));
+  runApp(MaterialApp(home: Page1(), debugShowCheckedModeBanner: false));
 }
