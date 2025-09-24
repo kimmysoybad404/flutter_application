b5f5521b11d6d4ce3d7aa54c14fa602e6fdcae38
Dinozexe <Kittamat34@gmail.com>
2025-09-25 00:19:21 +0700

    MainNaja

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application/week04/assignment1.dart';
-
 
 void main() {
-  runApp(MaterialApp(home: 
-      Assignment1(), debugShowCheckedModeBanner: false,
-    ),
-  );
+  runApp(MaterialApp(home: Scaffold(), debugShowCheckedModeBanner: false));
 }
