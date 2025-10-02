36b7fa53db627512030441ff3080f474f98221a0
Dinozexe <Kittamat34@gmail.com>
2025-10-03 01:16:10 +0700

    Load Data Form RealTime DataBase And Set Time date

--- lib/SmartHomeNaja/Page/Homepage.dart
+++ lib/SmartHomeNaja/Page/Homepage.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'dart:async';
+import 'package:intl/intl.dart';
 
 class Homepage extends StatefulWidget {
   const Homepage({super.key});
@@ -8,6 +11,13 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
+  String day = "--/--/--";
+  String time = "--:--:--";
+  String temperature = "-- °C";
+
+  final dbRef = FirebaseDatabase.instance.ref();
+  Timer? _timer;
+
   List<Map<String, dynamic>> devices = [
     {"name": "LED", "room": "Bedroom", "icon": Icons.lightbulb, "isOn": false},
     {"name": "Fan", "room": "Living Room", "icon": Icons.air, "isOn": false},
@@ -19,6 +29,66 @@ class _HomepageState extends State<Homepage> {
     },
   ];
 
+  void startClock() {
+    _timer = Timer.periodic(const Duration(seconds: 1), (_) {
+      final now = DateTime.now();
+      setState(() {
+        day = DateFormat("d EEEE MMMM yyyy").format(now);
+        time = DateFormat("HH:mm:ss").format(now);
+      });
+    });
+  }
+
+  String getTodayPath() {
+    final now = DateTime.now();
+    return DateFormat("yyyy-MM-dd").format(now);
+  }
+
+  void getLatestWeather() {
+    final todayPath = getTodayPath();
+    final weatherRef = dbRef.child("weather/$todayPath");
+
+    weatherRef.limitToLast(1).onValue.listen((event) {
+      if (event.snapshot.value != null) {
+        final raw = event.snapshot.value as Map<Object?, Object?>;
+        final data = raw.map(
+          (key, value) => MapEntry(key.toString(), value.toString()),
+        );
+
+        String lastValue = data.values.last.toString();
+
+        setState(() {
+          temperature = lastValue;
+        });
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    startClock();
+    getLatestWeather();
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
+  }
+
+  IconData getWeatherIcon() {
+    final hour = DateTime.now().hour;
+
+    if (hour >= 6 && hour < 18) {
+      return Icons.wb_sunny;
+    } else if (hour >= 18 && hour < 20) {
+      return Icons.cloud;
+    } else {
+      return Icons.nightlight_round;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,15 +96,18 @@ class _HomepageState extends State<Homepage> {
       appBar: AppBar(
         centerTitle: true,
         backgroundColor: const Color.fromARGB(136, 240, 240, 240),
-        title: const Text("Home"),
+        title: const Text(
+          "Home",
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
       ),
       body: Column(
         children: [
           const SizedBox(height: 20),
           Center(
             child: SizedBox(
-              height: 80,
-              width: 320,
+              height: 100,
+              width: 340,
               child: Container(
                 padding: const EdgeInsets.symmetric(
                   horizontal: 20,
@@ -46,25 +119,31 @@ class _HomepageState extends State<Homepage> {
                 ),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: const [
-                    Icon(Icons.cloud, color: Colors.white, size: 50),
+                  children: [
+                    Icon(getWeatherIcon(), color: Colors.white, size: 50),
                     Column(
                       mainAxisAlignment: MainAxisAlignment.center,
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Text(
-                          "Day",
-                          style: TextStyle(color: Colors.white, fontSize: 18),
+                          day,
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                          ),
                         ),
                         Text(
-                          "Time",
-                          style: TextStyle(color: Colors.white, fontSize: 14),
+                          "Time : $time",
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 14,
+                          ),
                         ),
                       ],
                     ),
                     Text(
-                      "28°C",
-                      style: TextStyle(
+                      temperature,
+                      style: const TextStyle(
                         color: Colors.white,
                         fontSize: 20,
                         fontWeight: FontWeight.bold,
@@ -115,7 +194,6 @@ class _HomepageState extends State<Homepage> {
                             ),
                           ),
                           const Spacer(),
-
                           SwitchTheme(
                             data: SwitchThemeData(
                               trackColor: MaterialStateProperty.resolveWith((
@@ -124,30 +202,24 @@ class _HomepageState extends State<Homepage> {
                                 if (states.contains(MaterialState.selected)) {
                                   return Colors.blue;
                                 }
-                                return const Color.fromARGB(
-                                  255,
-                                  207,
-                                  207,
-                                  207,
-                                ); 
+                                return const Color.fromARGB(255, 207, 207, 207);
                               }),
                               thumbColor: MaterialStateProperty.all(
                                 Colors.white,
-                              ), 
+                              ),
                               trackOutlineColor:
                                   MaterialStateProperty.resolveWith((states) {
                                     if (states.contains(
                                       MaterialState.selected,
                                     )) {
-                                      return Colors
-                                          .blueAccent; 
+                                      return Colors.blueAccent;
                                     }
                                     return const Color.fromARGB(
                                       255,
                                       207,
                                       207,
                                       207,
-                                    ); 
+                                    );
                                   }),
                             ),
                             child: Switch(
--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,25 @@ import 'package:flutter_application/Learn/ChangeImage.dart';
 import 'package:flutter_application/SmartHomeNaja/Main.dart';
 import 'package:flutter_application/W7/Page1.dart' hide Page1;
 import 'package:flutter_application/Test/Fishing.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
-  runApp(MaterialApp(home: Main(), debugShowCheckedModeBanner: false));
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,  
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Main(), 
+    );
+  }
 }
