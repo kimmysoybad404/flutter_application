3dc889d01f3fd2b4846f28d7c14607d1639b71c0
Kittinan <6631501009@lamduan.mfu.ac.th>
2025-10-09 13:20:08 +0700

    test

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'package:flutter_application/week06/login_dynamic.dart';
+
 
 
 
@@ -9,7 +9,7 @@ import 'package:flutter_application/week06/login_dynamic.dart';
 
 void main() {
   runApp(MaterialApp(home: 
-        LoginDN(), debugShowCheckedModeBanner: false,
+        (), debugShowCheckedModeBanner: false,
     ),
   );
 }
