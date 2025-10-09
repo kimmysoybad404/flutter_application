dd4fad3229fd2b5e6febfe7e815f19d701015e43
YIMYUAN <6631501003@lamduan.mfu.ac.th>
2025-10-10 02:25:07 +0700

    add Ass3

--- lib/week07/page/Ass3.dart
+++ lib/week07/page/Ass3.dart
@@ -10,17 +10,24 @@ class Ass3 extends StatefulWidget {
 class _Ass3State extends State<Ass3> {
   bool sw = false;
   double sliderValue2 = 0;
-  String TextSugar = "none";
+  String TextSugar = "no";
+  String LabelSugar = "none";
   String TypeHorC = "Hot";
   String TextCoffee = "Latte";
   int Baht = 35;
   int TypeCoffee = 0;
   String ThankText = "";
 
-  List ImageCoffee = [
+  List<String> ImageCoffee = [
     "assets/images/latehot.jpg",
     "assets/images/Americanohot.jpg",
     "assets/images/Cappuccino-exc.webp",
+  ];
+
+  List<String> ImageCoffeecold = [
+    "assets/images/Iced_Latte.jpg",
+    "assets/images/Americanocold.jpg",
+    "assets/images/Cappuccinocold.jpg",
   ];
 
   List Coffee = ["Latte 35", "Americano 30", "Cappuccino 40"];
@@ -34,6 +41,14 @@ class _Ass3State extends State<Ass3> {
         TypeHorC = "Hot";
       }
     });
+  }
+
+  List<String> checkhotandcold() {
+    if (sw == true) {
+      return ImageCoffeecold;
+    } else {
+      return ImageCoffee;
+    }
   }
 
   void SelectCofffe(int? value) {
@@ -53,20 +68,40 @@ class _Ass3State extends State<Ass3> {
   }
 
   void showAlert() async {
+    int totalPrice = Baht;
     if (sw == true) {
-      Baht += 5;
+      totalPrice += 5;
     }
+
     await showDialog(
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
           title: Text('Your order'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              SizedBox(
+                width: 150,
+                height: 150,
+                child: Image.asset(
+                  checkhotandcold()[TypeCoffee],
+                  fit: BoxFit.cover,
+                ),
+              ),
+              const SizedBox(height: 10),
+              Text(
+                '$TypeHorC $TextCoffee coffee with $TextSugar sugar.\nPrice = $totalPrice',
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
           actions: [
             TextButton(
               onPressed: () {
                 Navigator.of(context).pop();
               },
-              child: Text('Cancel'),
+              child: const Text('Cancel'),
             ),
             TextButton(
               onPressed: () {
@@ -75,20 +110,9 @@ class _Ass3State extends State<Ass3> {
                   ThankText = "Thank you for your order!";
                 });
               },
-              child: Text('Ok'),
+              child: const Text('Ok'),
             ),
           ],
-          content: Column(
-            children: [
-              Spacer(),
-              Image.asset(ImageCoffee[TypeCoffee]),
-              Spacer(),
-              Text(
-                '$TypeHorC $TextCoffee coffee with $TextSugar sugar. Price = $Baht',
-              ),
-              Spacer(),
-            ],
-          ),
         );
       },
     );
@@ -128,13 +152,25 @@ class _Ass3State extends State<Ass3> {
               child: Text("Your Order", style: TextStyle(fontSize: 30)),
             ),
 
+            const SizedBox(height: 10),
+
+            Padding(
+              padding: const EdgeInsets.only(left: 15),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "Coffee",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
             Column(children: radios),
 
             Padding(
               padding: const EdgeInsets.all(20.0),
               child: Row(
                 children: [
-                  Text("Type"),
+                  Text("Type", style: TextStyle(fontWeight: FontWeight.bold)),
                   Spacer(),
                   Text("Hot"),
                   Switch(value: sw, onChanged: SelectType),
@@ -144,25 +180,36 @@ class _Ass3State extends State<Ass3> {
             ),
 
             Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Text("Sugar"),
-                const SizedBox(width: 20),
+                Padding(
+                  padding: const EdgeInsets.only(left: 20),
+                  child: Text(
+                    "Sugar",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                const SizedBox(width: 50),
                 Text("None"),
                 Slider(
                   min: 0,
                   max: 100,
                   value: sliderValue2,
                   divisions: 2,
-                  label: TextSugar,
+                  label: LabelSugar,
                   onChanged: (value) {
                     sliderValue2 = value;
                     setState(() {
                       if (value == 0) {
-                        TextSugar = "none";
+                        TextSugar = "no";
+                        LabelSugar = "none";
                       } else if (value == 50) {
                         TextSugar = "less";
+                        LabelSugar = "less";
                       } else if (value == 100) {
                         TextSugar = "normal";
+                        LabelSugar = "normal";
                       }
                     });
                   },
@@ -170,7 +217,6 @@ class _Ass3State extends State<Ass3> {
                 Text("Normal"),
               ],
             ),
-
             const SizedBox(height: 20),
 
             ElevatedButton(
