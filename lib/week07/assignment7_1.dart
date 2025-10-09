df9e91572969cca456df72dd9233e523e10de03f
Kimmysoybad <6631501008@lamduan.mfu.ac.th>
2025-10-09 16:28:46 +0700

    problem cant show date at start

--- lib/week07/assignment7_1.dart
+++ lib/week07/assignment7_1.dart
@@ -8,15 +8,76 @@ class Assignment71 extends StatefulWidget {
 }
 
 class _Assignment71State extends State<Assignment71> {
+  String _fromdate = '';
+  String _todate = '';
+
+  Future<DateTime?> datetime() async {
+    DateTime? dt = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(DateTime.now().year, 1, 1),
+      lastDate: DateTime(DateTime.now().year, 12, 31),
+    );
+    return dt;
+  }
+
+  void today() {
+    DateTime dt = datetime() as DateTime;
+    setState(() {
+      _todate = '${dt.day}/${dt.month}/${dt.year}';
+      // _date = dt.toString();
+    });
+  }
+
+  void selectFromDate() async {
+    DateTime dt = datetime() as DateTime;
+    setState(() {
+      _fromdate = '${dt.day}/${dt.month}/${dt.year}';
+      // _date = dt.toString();
+    });
+  }
+
+  void selectToDate() async {
+    DateTime dt = datetime() as DateTime;
+    setState(() {
+      _todate = '${dt.day}/${dt.month}/${dt.year}';
+      // _date = dt.toString();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("DatePicker Demo"),
-      ),
+      appBar: AppBar(title: const Text('DatePicker Demo')),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                FilledButton.icon(
+                  onPressed: selectFromDate,
+                  icon: const Icon(Icons.calendar_today),
+                  label: const Text('From'),
+                ),
+                SizedBox(width: 10),
+                Text(_fromdate),
+              ],
+            ),
 
-      body: Column(
-        
+            Row(
+              children: [
+                FilledButton.icon(
+                  onPressed: selectToDate,
+                  icon: const Icon(Icons.calendar_today),
+                  label: const Text('To'),
+                ),
+                SizedBox(width: 10),
+                Text(_todate),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
