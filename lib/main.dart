9dfd0372429636bd323da52ffa4352c0f82d429f
Kimmysoybad <6631501008@lamduan.mfu.ac.th>
2025-10-12 15:43:59 +0700

    initial assignment 7.2

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application/week07/assignment7_1.dart';
+import 'package:flutter_application/week07/assignment7_2.dart';
 
 
 void main() {
   runApp(MaterialApp(home: 
-      Assignment71(), debugShowCheckedModeBanner: false,
+      Assignment72(), debugShowCheckedModeBanner: false,
     ),
   );
 }
---
+++ lib/week07/assignment7_2.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Assignment72 extends StatefulWidget {
+  const Assignment72({super.key});
+
+  @override
+  State<Assignment72> createState() => _Assignment72State();
+}
+
+class _Assignment72State extends State<Assignment72> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
